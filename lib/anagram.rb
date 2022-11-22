019203f7d5a26a53a372aed585b16f866b8fd86d
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-11-22 18:26:08 -0500

    commit tree 786185408dd2b8e597bfb9eed84a4bcda20f7b25

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(new_word)
+        @word = new_word
+    end
+
+    def match(array_of_anagrams)
+        array_of_anagrams.filter do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    end
+end
+
+
